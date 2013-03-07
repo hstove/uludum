ff7c309b31720e837c2f1a3cfdf49a26764c77d5
Hank Stoever <hstove@gmail.com>
2013-03-07 14:40:24 -0800

    dont precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Ludum
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #heroku compile error http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
