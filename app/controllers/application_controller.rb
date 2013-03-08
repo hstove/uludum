255200f8805ecf0c27ed84418fa13ece134ff76f
Hank Stoever <hstove@gmail.com>
2013-03-07 21:02:32 -0800

    alerts, better redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to root_url, alert: "The content you wish to request is unavailable."
   end
 
   def taught? course
