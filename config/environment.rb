8faaf0c1013d3a4f41f3e6d821aca212aad498b0
Hank Stoever <hstove@gmail.com>
2013-04-06 15:23:19 -0700

    move to production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,3 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,14 @@ Ludum::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.logger = Logger.new(STDOUT)
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
