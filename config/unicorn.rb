754737a8a3ac88e7c3b1d7b025dd329eafab6259
Hank Stoever <hstove@gmail.com>
2013-05-13 18:24:14 -0700

    unicorn broken

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,10 +10,10 @@ before_fork do |server, worker|
   end
  
   # If you are using Redis but not Resque, change this
-  if defined?(Afterparty)
-    Rails.configuration.queue.consumer.shutdown
-    Rails.logger.info('Disconnected from Redis')
-  end
+  # if defined?(Afterparty)
+  #   Rails.configuration.queue.consumer.shutdown
+  #   Rails.logger.info('Disconnected from Redis')
+  # end
  
   sleep 1
 end
@@ -25,12 +25,12 @@ after_fork do |server, worker|
     Rails.logger.info('Connected to ActiveRecord')
   end
  
-  # If you are using Redis but not Resque, change this
-  if defined?(Afterparty)
-    require 'open-uri'
-    uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
-    Rails.configuration.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    Rails.configuration.queue.redis = Rails.configuration.redis
-    Rails.logger.info('Connected to Redis')
-  end
+  # # If you are using Redis but not Resque, change this
+  # if defined?(Afterparty)
+  #   require 'open-uri'
+  #   uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+  #   Rails.configuration.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  #   Rails.configuration.queue.redis = Rails.configuration.redis
+  #   Rails.logger.info('Connected to Redis')
+  # end
 end
