f5ee983a4ab34b4ec26bda61676e7ff5e9076a79
Hank Stoever <hstove@gmail.com>
2013-05-14 11:08:46 -0700

    fix staging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,19 @@ class PagesController < ApplicationController
   def show
     page = params[:template]
     track "visit #{page} page" unless page == "about"
-    Rails.configuration.queue << TestJob.new
+    # Rails.configuration.queue << TestJob.new
     render page
   end
 
   class TestJob
     attr_accessor :execute_at
     def initialize
-      @execute_at = Time.now + 10.seconds
+      @mail = UserMailer.welcome_email(User.find(1))
+      @execute_at = Time.now + 1.minute
     end
     
     def run
+      @mail.deliver
       ap "running a job!"
     end
   end
--- config/initializers/amazon_flex_pay.rb
+++
@@ -1,3 +0,0 @@
-AmazonFlexPay.access_key = ENV['U_AWS_ACCESS_KEY_ID']
-AmazonFlexPay.secret_key = ENV['U_AWS_SECRET_KEY']
-AmazonFlexPay.go_live! if Rails.env.production?
