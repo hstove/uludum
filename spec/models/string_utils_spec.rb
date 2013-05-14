e3885bd4ad7118dc5dab81de9d66d8b282ca9b26
Hank Stoever <hstove@gmail.com>
2013-05-14 09:33:17 -0700

    check if refinements are possible

--- spec/models/string_utils_spec.rb
+++ spec/models/string_utils_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 
 describe StringUtils do
-  using StringUtils
+  if respond_to? :using
+    using StringUtils
 
-  it "knows what is numeric" do
-    n?(1.25).should == true
-    n?(1).should == true
-    n?('1.5e4').should == true
+    it "knows what is numeric" do
+      n?(1.25).should == true
+      n?(1).should == true
+      n?('1.5e4').should == true
 
-    n?('helo').should == false
-    n?('1a').should == false
-  end
+      n?('helo').should == false
+      n?('1a').should == false
+    end
 
-  def n?(input)
-    input.to_s.is_numeric?
+    def n?(input)
+      input.to_s.is_numeric?
+    end
   end
 end
