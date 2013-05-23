962fa7204e684e3826a5abe55106eae20144abb9
Hank Stoever <hstove@gmail.com>
2013-05-23 11:33:13 -0700

    responsive emailing and secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,8 +1,10 @@
-# Be sure to restart your server when you modify this file.
+if Rails.env.development? or Rails.env.test?
+  key = ('x' * 30)
+  base = ('y' * 30)
+else
+  key = ENV['SECRET_TOKEN']
+  base = ENV['SECRET_BASE']
+end
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ludum::Application.config.secret_token = '6a342f4429ab503ae708638dd106b0e873e11d3a0628ee86fe30c5cbeee247fa5a15d30a920feaf1523bc45cdcbadc430cc636655e1b1c1f3fe22da0b0201bff'
-Ludum::Application.config.secret_key_base = 'blahksdjfkdsajfiewifhweihr2i3ti2i9p45904'
+Ludum::Application.config.secret_token = key
+Ludum::Application.config.secret_key_base = base
