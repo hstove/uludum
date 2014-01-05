838c6ee878cba71e06ea4e2ad0a5abacc0b7cdd0
Hank Stoever <hstove@gmail.com>
2014-01-04 20:46:47 -0800

    edit your bitcoin address in users#edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
 
   # new columns need to be added here to be writable through mass assignment
   attr_accessible :username, :email, :password, :password_confirmation, :about_me, :teacher_description, :avatar_url, :show_email
-  attr_accessible :password_reset_token
+  attr_accessible :password_reset_token, :bitcoin_address
 
   attr_accessor :password
   before_save :prepare_password
