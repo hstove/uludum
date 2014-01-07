6b392c6ebeb49e742ab6350972f2eb51b18d77cc
Hank Stoever <hstove@gmail.com>
2014-01-06 19:06:05 -0800

    adds 'random' factory sequence

--- test/factories.rb
+++ test/factories.rb
@@ -20,4 +20,8 @@ FactoryGirl.define do
   sequence :body do |n|
     LoremIpsum::Generator.new.generate({})
   end
+
+  sequence :random do
+    (0...34).map { (65 + rand(26)).chr }.join
+  end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -7,6 +7,6 @@ FactoryGirl.define do
     password "password"
     sequence(:recipient_token) {|n| "token#{n}#{(rand*1000).to_i}" }
     stripe_key "sk_test_XF2SctReftg7417qgx56Iy6R"
-    bitcoin_address { (0...34).map { (65 + rand(26)).chr }.join }
+    bitcoin_address FactoryGirl.generate(:random)
   end
 end
