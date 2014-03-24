0d435a4dcad60e8e62a18ce18da9258175eaeb9b
Hank Stoever <hstove@gmail.com>
2014-03-23 17:17:48 -0700

    make sure notification isn't sent to self

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,8 +13,16 @@ describe Comment do
 
   it "sends an email to other members of the discussion" do
     other_comment = create :comment, commentable: commentable
+    reset_email
     comment.save
     notifier = last_email
     last_email.to.should include(other_comment.user.email)
+    notifications = ActionMailer::Base.deliveries.select do |mail|
+      mail.subject.include? "has a new comment"
+    end
+    notifications.size.should eql 2
+    notifications.select do |mail|
+      mail.to.first == comment.user.email
+    end.size.should eql 0
   end
 end
