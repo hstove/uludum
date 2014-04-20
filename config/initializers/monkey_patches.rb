9dc8e6978c7b5f08a5ff90a3966a23b7c807e981
Hank Stoever <hstove@gmail.com>
2014-04-19 17:25:59 -0700

    allow headers in user content

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -9,7 +9,8 @@ class String
 
   def sanitize!
     options = Sanitize::Config::BASIC
-    options[:elements].concat %w[img div label input textarea utensil]
+    options[:elements].concat %w[img div utensil]
+    options[:elements].concat %w[h1 h2 h3 h4 h5 h6]
     options[:add_attributes]['a']['target'] = "_blank"
     options[:attributes]['img'] = ['src']
     options[:attributes]['utensil'] = ['data-json']
