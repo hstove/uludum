517f16fc8daf7e59b82f671161c8e0fbfe46ce40
hank <hstove@gmail.com>
2014-09-29 23:32:08 -0700

    better homepage

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -17,4 +17,16 @@ module PagesHelper
     course = Course.exists?(id) ? Course.find(id) : Course.first
     link_to title, course
   end
+
+  def how_it_works(step_num, &block)
+    haml_tag '.row' do
+      haml_tag '.span1' do
+        haml_tag('span.intro-step', step_num)
+      end
+      haml_tag '.span7', &block
+    end
+    haml_tag "br"
+    haml_tag ".break-dark"
+    haml_tag "br"
+  end
 end
